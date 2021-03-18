a251c6c8848b1ccfdb4a2987c2c09449a5b048f6
dalibor.koenig <dalibor.koenig@gmail.com>
2021-03-18 15:28:30 +0100

    done

--- script.js
+++ script.js
@@ -1,13 +1,19 @@
-function getRandomImage() {
-    let num = Math.ceil(Math.random()*20)
-    let randomImage = `url(./assets/background/background_${num}.jpg)`;
-    return randomImage;
-};
+// function getRandomImage() {
+//     let num = Math.ceil(Math.random()*20)
+//     let randomImage = `url(./assets/background/background_${num}.jpg)`;
+//     return randomImage;
+// };
+
+
 
 function randomImg() {
-    document.body.style.background = getRandomImage();
-    document.getElementById('mobile').style.background = getRandomImage();
+    let num = Math.ceil(Math.random()*20)
+    let randomImage = `url(./assets/background/background_${num}.jpg)`;
+    document.body.style.background = randomImage;
+    document.getElementById('mobile').style.background = randomImage;
 }
 
 
-
+// function randomImg() {
+//     document.querySelectorAll("#body, #mobile").style.background = getRandomImage();
+// }
