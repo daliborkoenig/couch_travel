81d29674e5a99a0d2e5be1b56d0a6fbf019b789d
Dalibor Koenig <dalibor.koenig@gmail.com>
2021-03-18 15:30:43 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,8 @@
-function getRandomImage() {
+function randomImg() {
     let num = Math.ceil(Math.random()*20)
     let randomImage = `url(./assets/background/background_${num}.jpg)`;
-    return randomImage;
-};
-
-function randomImg() {
-    document.body.style.background = getRandomImage();
-    document.getElementById('mobile').style.background = getRandomImage();
+    document.body.style.background = randomImage;
+    document.getElementById('mobile').style.background = randomImage;
 }
 
 
